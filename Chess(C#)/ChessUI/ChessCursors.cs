31fd332987a1bc49dcefed4888d87e444aa982df
Yuuki Nishida <144459032+yuuknishida@users.noreply.github.com>
2024-10-06 13:18:18 -0400

    Add files via upload

---
+++ Chess(C#)/ChessUI/ChessCursors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ChessUI
+{
+    public static class ChessCursors
+    {
+        public static readonly Cursor WhiteCursor = LoadCursor("Assets/CursorW.cur");
+        public static readonly Cursor BlackCursor = LoadCursor("Assets/CursorB.cur");
+        private static Cursor LoadCursor(string filepath)
+        {
+            Stream stream = Application.GetResourceStream(new Uri(filepath, UriKind.Relative)).Stream;
+            return new Cursor(stream, true);
+        }
+    }
+}
