2dd6f1ab34fd4584d7cddb5b18178a52fdba04c1
Yuuki Nishida <144459032+yuuknishida@users.noreply.github.com>
2024-10-06 11:43:11 -0400

    Add files via upload

---
+++ ChessLogic/Pieces/Queen.cs
@@ -0,0 +1,33 @@
+﻿namespace ChessLogic
+{
+    public class Queen : Piece
+    {
+        public override PieceType Type => PieceType.Queen;
+        public override Player Color { get; }
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.North,
+            Direction.South,
+            Direction.East,
+            Direction.West,
+            Direction.NorthWest,
+            Direction.NorthEast,
+            Direction.SouthEast,
+            Direction.SouthWest
+        };
+        public Queen(Player color)
+        {
+            Color = color;
+        }
+        public override Piece Copy()
+        {
+            Queen copy = new Queen(Color);
+            copy.HasMoved = HasMoved;
+            return copy;
+        }
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositionsInDirs(from, board, dirs).Select(to => new NormalMove(from, to));
+        }
+    }
+}
