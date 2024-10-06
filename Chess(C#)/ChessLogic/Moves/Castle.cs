670266b35d7c5c5c6a13750a70183dbaa39d99a9
Yuuki Nishida <144459032+yuuknishida@users.noreply.github.com>
2024-10-06 13:16:10 -0400

    Add files via upload

---
+++ Chess(C#)/ChessLogic/Moves/Castle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic.Moves
+{
+    public class Castle : Move
+    {
+        public override MoveType Type { get; }
+        public override Position FromPos { get; }
+        public override Position ToPos { get; }
+
+        private readonly Direction kingMoveDir;
+        private readonly Position rookFromPos;
+        private readonly Position rookToPos;
+        public Castle(MoveType type, Position kingPos)
+        {
+            Type = type;
+            FromPos = kingPos;
+
+            if (type == MoveType.CastleKS)
+            {
+                kingMoveDir = Direction.East;
+                ToPos = new Position(kingPos.Row, 6);
+                rookFromPos = new Position(kingPos.Row, 7);
+                rookToPos = new Position(kingPos.Row, 5);
+            }
+            else if (type == MoveType.CastleQS)
+            {
+                kingMoveDir = Direction.West;
+                ToPos = new Position(kingPos.Row, 2);
+                rookFromPos = new Position(kingPos.Row, 0);
+                rookToPos = new Position(kingPos.Row, 3);
+            }
+        }
+        public override bool Execute(Board board)
+        {
+            new NormalMove(FromPos, ToPos).Execute(board);
+            new NormalMove(rookFromPos, rookToPos).Execute(board);
+
+            return false;
+        }
+        public override bool IsLegal(Board board)
+        {
+            Player player = board[FromPos].Color;
+
+            if (board.IsInCheck(player))
+            {
+                return false;
+            }
+
+            Board copy = board.Copy();
+            Position kingPosIncopy = FromPos;
+
+            for (int i = 0; i<2; i++)
+            {
+                new NormalMove(kingPosIncopy, kingPosIncopy + kingMoveDir).Execute(copy);
+                kingPosIncopy += kingMoveDir;
+
+                if (copy.IsInCheck(player))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
